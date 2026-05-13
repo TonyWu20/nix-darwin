33f4ba774a132d17cc4e575db6ad44bca85b0fab
TonyWu20 <tony.w21@gmail.com>
2026-05-14 00:41:06 +0800

    Disabled unfrequently used skills

--- claude-code/default.nix
+++ claude-code/default.nix
@@ -8,12 +8,10 @@
     package = nixpkgs-cc-patch.claude-code;
     skills = {
       first-principle = ./first-principles-skill;
-      rust-api-doc = ./rust-api-doc;
-      humanizer = ./humanizer;
-      humanizer-zh = ./humanizer-zh;
-    };
-    hooks = {
-      five_mins = builtins.readFile ./five_min.sh;
+      #rust-api-doc = ./rust-api-doc;
+      #humanizer = ./humanizer;
+      #humanizer-zh = ./humanizer-zh;
+      socrates-skill = ./socrates-skill;
     };
   };
 }
