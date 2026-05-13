0056a99f37266b9be0866583a90f8c97d5f78bf8
TonyWu20 <tony.w21@gmail.com>
2026-05-14 00:38:30 +0800

    Remove the line of `run-shell` to trigger `tmux-cpu` plugin: It will spawn processes everytime a tmux client is attached and eventually exhaust the CPU

--- tmux/default.nix
+++ tmux/default.nix
@@ -6,6 +6,7 @@
     extraConfig = builtins.concatStringsSep "\n" [
       (builtins.readFile ./tmux.conf)
       "set -g visual-bell both"
+      "set -g focus-events on"
       "set -g bell-action other"
       "setw -g monitor-activity on"
       ''
@@ -15,7 +16,6 @@
         # Toggle input and force a border refresh (Prefix + X)
         bind X run-shell -C "select-pane -#{?pane_input_off,e,d}; refresh-client -S"
       ''
-      "run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux"
     ];
     terminal = "tmux-256color";
     plugins = with pkgs; [
