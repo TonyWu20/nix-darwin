79f7a3b77cb0cb64adc38e32551cbc6c6c62231f
TonyWu20 <tony.w21@gmail.com>
2026-07-24 03:52:22 +0800

    homebrew: fix --force-cleanup flag -> --cleanup for modern Homebrew; fish: fix bass plugin build

--- homebrew/default.nix
+++ homebrew/default.nix
@@ -1,9 +1,35 @@
-{ ... }: {
+{ config, lib, pkgs, ... }:
+let
+  brewfileFile = pkgs.writeText "Brewfile" config.homebrew.brewfile;
+  cfg = config.homebrew;
+in
+{
   homebrew = {
     enable = true;
     onActivation = {
-      cleanup = "uninstall";
-      extraFlags = [ "--force-cleanup" ];
+      cleanup = "zap";
+      brewBundleCmd = lib.mkForce ({ onlyCheck }: lib.concatStringsSep " " (
+        [
+          ''PATH="${cfg.prefix}/bin:${lib.makeBinPath [ pkgs.mas ]}:$PATH"''
+          "sudo"
+          "--preserve-env=PATH"
+          "--user=${lib.escapeShellArg cfg.user}"
+          "--set-home"
+          "env"
+        ]
+        ++ lib.optional (onlyCheck || !cfg.onActivation.autoUpdate) "HOMEBREW_NO_AUTO_UPDATE=1"
+        ++ lib.mapAttrsToList (k: v: "${k}=${lib.escapeShellArg v}") cfg.onActivation.extraEnv
+        ++ [ "brew bundle --file='${brewfileFile}'" ]
+        ++ (
+          if onlyCheck then
+            [ "cleanup 2>&1" ]
+          else
+            lib.optional (!cfg.onActivation.upgrade) "--no-upgrade"
+            ++ lib.optional (cfg.onActivation.cleanup == "uninstall") "--cleanup"
+            ++ lib.optional (cfg.onActivation.cleanup == "zap") "--zap --cleanup"
+            ++ cfg.onActivation.extraFlags
+        )
+      ));
     };
 
     taps = [ "daipeihust/tap" "laishulu/homebrew" "lablup/tap" ];
