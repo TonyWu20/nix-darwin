5cf996b1efaf8308e1c533efcb650c0c46111ac5
TonyWu20 <tony.w21@gmail.com>
2026-09-03 03:26:53 +0800

    Point nix-pro5000 at 10.147.17.8

--- ssh/air.nix
+++ ssh/air.nix
@@ -57,8 +57,7 @@
       nix-pro5000 = {
         host = "nixos-pro5000";
         user = "tony";
-        hostname = "10.0.0.6";
-        proxyJump = "nix";
+        hostname = "10.147.17.8";
       };
     };
   };
